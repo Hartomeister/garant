a5bdab6e5799189271e95372259093fcbd701ec0
smxxkey <99882370+smxxkey@users.noreply.github.com>
2022-07-31 11:34:08 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,17 +1,17 @@
-TOKEN = '' #����ܧ֧� �ҧ���
-admin =  #Chat_id �ѧէާڧߧ� 1
-admin2 =  #Chat_id �ѧէާڧߧ� 2
-chat_bota = '' #������ݧܧ� �ߧ� �ӧѧ� ��ѧ�.
-instruction = '-' #������ݧܧ� �ߧ� �ڧߧ����ܧ�ڧ�, �ާ�اߧ� �ߧѧ�ڧ�ѧ�� �� telegra.ph
-nicknameadm = '' #���ڧ� �ѧէާڧߧ� �ҧ֧� @
-procent =  #������֧ߧ� �ܧ�ާڧ��ڧ� ��֧�ӧڧ��
-chat_id_bot =   #ID �ܧ�է� �ҧ�է֧� �����ѧӧݧ֧ߧ� ����ҧ�֧ߧڧ� ��� ����֧�ߧ��� ��է֧ݧܧѧ�(���� �ѧէާڧߧ�ܧ��, �� �էݧ� ���ݧ�٧�ӧѧ�֧ݧ֧�)
-number_qiwi = '' #����ާ֧� �ܧڧӧ�
-db = 'db.db' #���ѧ���ݧ�ا֧ߧڧ� �ҧѧ٧� �էѧߧߧ���, �֧�ݧ� �ӧ� �ߧ� �٧ߧѧ֧�� ���� ����, �ߧ� ����ԧѧۧ��!
-token_qiwi = '' # ����ܧ֧� �ܧڧӧ�
+TOKEN = '' #妥抉抗快扶 忌抉找忘
+admin =  #Chat_id 忘忱技我扶忘 1
+admin2 =  #Chat_id 忘忱技我扶忘 2
+chat_bota = '' #妊扼抑抖抗忘 扶忘 志忘扮 折忘找.
+instruction = '-' #妊扼抑抖抗忘 扶忘 我扶扼找把批抗扯我攻, 技抉忪扶抉 扶忘扭我扼忘找抆 志 telegra.ph
+nicknameadm = '' #妖我抗 忘忱技我扶忘 忌快戒 @
+procent =  #妤把抉扯快扶找 抗抉技我扼扼我我 扼快把志我扼忘
+chat_id_bot = 
+number_qiwi = '' 
+db = 'db.db' 
+token_qiwi = '' 
 
-replenish = '�7�2�1�5 ������ݧߧ֧ߧڧ� �ҧѧݧѧߧ��\n\n' \
-                    '�0�7 Qiwi \n\n' \
-                    '�9�7 ����ާ֧�(Qiwi) <b><code> +7xxxxxxxxxx</code>\n' \
-                    '�9�7 ����ާ֧ߧ�ѧ�ڧ�  <code>{code}</code></b>\n' \
-                    '�9�7 ���� 15 000 ���ҧݧ֧�!'
+replenish = '�7�2�1�5 妤抉扭抉抖扶快扶我快 忌忘抖忘扶扼忘\n\n' \
+                    '�0�7 Qiwi \n\n' \
+                    '�9�7 妖抉技快把(Qiwi) <b><code> +7xxxxxxxxxx</code>\n' \
+                    '�9�7 妞抉技快扶找忘把我抄  <code>{code}</code></b>\n' \
+                    '�9�7 坏抉 15 000 把批忌抖快抄!'
