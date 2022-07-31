2c00e4ac36c941328da1fd658c607d46410f9a58
smxxkey <99882370+smxxkey@users.noreply.github.com>
2022-07-31 11:36:12 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
 TOKEN = '' #妥抉抗快扶 忌抉找忘
-admin =  #Chat_id 忘忱技我扶忘 1
-admin2 =  #Chat_id 忘忱技我扶忘 2
+admin =  5206676272
+admin2 =  5206676272
 chat_bota = '' #妊扼抑抖抗忘 扶忘 志忘扮 折忘找.
 instruction = '-' #妊扼抑抖抗忘 扶忘 我扶扼找把批抗扯我攻, 技抉忪扶抉 扶忘扭我扼忘找抆 志 telegra.ph
-nicknameadm = '' #妖我抗 忘忱技我扶忘 忌快戒 @
+nicknameadm = 'youkea' #妖我抗 忘忱技我扶忘 忌快戒 @
 procent =  #妤把抉扯快扶找 抗抉技我扼扼我我 扼快把志我扼忘
-chat_id_bot = 
-number_qiwi = '' 
+chat_id_bot = 5518179147
+number_qiwi = '79111222569' 
 db = 'db.db' 
-token_qiwi = '' 
+token_qiwi = '30af97cac8632f48e961e5c4f11655a4' 
 
 replenish = '�7�2�1�5 妤抉扭抉抖扶快扶我快 忌忘抖忘扶扼忘\n\n' \
                     '�0�7 Qiwi \n\n' \
